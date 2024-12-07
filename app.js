d0d3d6f2729230e1330e3e0d0b0326c29bf72eff
Andy Smith <andysmithfal@users.github.com>
2024-12-07 15:44:14 +0000

    publish vehicle health data

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
-const mqtt = require('mqtt');
+const mqtt = require("mqtt")
 const Polestar = require("@andysmithfal/polestar.js")
 
-require('dotenv').config();
+require("dotenv").config()
 
 const baseTopic = "polestar/"
 let vin = null
 
+const polestar = new Polestar(
+  process.env.POLESTAR_EMAIL,
+  process.env.POLESTAR_PASSWORD
+)
 
-const polestar = new Polestar(process.env.POLESTAR_EMAIL, process.env.POLESTAR_PASSWORD)
-
-const mqttClient = mqtt.connect(`mqtt://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`, {
+const mqttClient = mqtt.connect(
+  `mqtt://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`,
+  {
     username: process.env.MQTT_USER,
-    password: process.env.MQTT_PASSWORD
-})
+    password: process.env.MQTT_PASSWORD,
+  }
+)
 
 async function getCarInfo() {
-    try{
-        const battery = await polestar.getBattery()
-        const odometer = await polestar.getOdometer()
-        mqttClient.publish(baseTopic+vin+'/battery', JSON.stringify(battery))
-        mqttClient.publish(baseTopic+vin+'/odometer', JSON.stringify(odometer))
-    } catch(e){
-        console.log(e)
-    }
+  try {
+    const battery = await polestar.getBattery()
+    const odometer = await polestar.getOdometer()
+    const health = await polestar.getHealthData()
+    mqttClient.publish(baseTopic + vin + "/battery", JSON.stringify(battery))
+    mqttClient.publish(baseTopic + vin + "/odometer", JSON.stringify(odometer))
+    mqttClient.publish(baseTopic + vin + "/health", JSON.stringify(health))
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 async function main() {
-    try {
-        await polestar.login()
-        const vehicles = await polestar.getVehicles()
-        vin = vehicles[0].vin
-        await polestar.setVehicle(vin)
-        console.log("Successfully logged into Polestar account and set vehicle")
-        getCarInfo()
-        setInterval(getCarInfo, 60000)
-    } catch (e) {
-        throw new Error(e)
-    }
+  try {
+    await polestar.login()
+    const vehicles = await polestar.getVehicles()
+    vin = vehicles[0].vin
+    await polestar.setVehicle(vin)
+    console.log("Successfully logged into Polestar account and set vehicle")
+    getCarInfo()
+    setInterval(getCarInfo, 60000)
+  } catch (e) {
+    throw new Error(e)
+  }
 }
 
 main()
